fa36aa41bb014c80bc1f325d56c2e9a275caff59
sumeng <sumeng@pwnzen.com>
2018-08-28 14:35:03 +0800

    update

--- model_im.py
+++ model_im.py
@@ -31,7 +31,7 @@ FRIEND_TYPE_FOLLOW = 4
 FRIEND_TYPE_SPECAIL_FOLLOW = 5
 FRIEND_TYPE_MUTUAL_FOLLOW = 6
 FRIEND_TYPE_RECENT = 7
-FRIEND_TYPE_GH = 8
+FRIEND_TYPE_SUBSCRIBE = 8
 FRIEND_TYPE_STRANGER = 9
 
 CHAT_TYPE_FRIEND = 1
@@ -620,7 +620,7 @@ class GenerateModel(object):
             if row[15]:
                 user.Source.Value = row[15]
             if row[16]:
-                user.Deleted.Value = self._convert_deleted_status(row[16])
+                user.Deleted = self._convert_deleted_status(row[16])
             if row[0]:
                 user.ID.Value = row[0]
                 account_id = row[0]
@@ -688,7 +688,7 @@ class GenerateModel(object):
             if row[13]:
                 friend.Source.Value = row[13]
             if row[14]:
-                friend.Deleted.Value = self._convert_deleted_status(row[14])
+                friend.Deleted = self._convert_deleted_status(row[14])
             if row[0]:
                 friend.OwnerUserID.Value = row[0]
                 account_id = row[0]
@@ -752,7 +752,7 @@ class GenerateModel(object):
             if row[12]:
                 group.Source.Value = row[12]
             if row[13]:
-                group.Deleted.Value = self._convert_deleted_status(row[13])
+                group.Deleted = self._convert_deleted_status(row[13])
             if row[0]:
                 group.OwnerUserID.Value = row[0]
                 account_id = row[0]
@@ -777,7 +777,9 @@ class GenerateModel(object):
             if row[10]:
                 group.MemberMaxCount.Value = row[10]
             if row[11]:
-                group.JoinTime.Value = self._get_timestamp(row[11])
+                ts = self._get_timestamp(row[11])
+                if ts:
+                    group.JoinTime.Value = ts
             models.append(group)
 
             if account_id is not None and user_id is not None:
@@ -812,7 +814,7 @@ class GenerateModel(object):
             if row[14]:
                 message.Source.Value = row[14]
             if row[15]:
-                message.Deleted.Value = self._convert_deleted_status(row[15])
+                message.Deleted = self._convert_deleted_status(row[15])
             if row[0]:
                 message.OwnerUserID.Value = row[0]
                 account_id = row[0]
@@ -830,7 +832,9 @@ class GenerateModel(object):
             if row[11]:
                 message.Content.Value.Location.Value = self._get_location(row[11])
             if row[10]:
-                message.TimeStamp.Value = self._get_timestamp(row[10])
+                ts = self._get_timestamp(row[10])
+                if ts:
+                    message.TimeStamp.Value = ts
 
             msg_type = row[7]
             content = row[8]
@@ -870,7 +874,7 @@ class GenerateModel(object):
                 else:
                     chat = Generic.Chat()
                     chat.Source.Value = message.Source.Value
-                    chat.Deleted.Value = self._convert_deleted_status(0)
+                    chat.Deleted = self._convert_deleted_status(0)
                     chat.OwnerUserID.Value = account_id
                     chat.ChatId.Value = talker_id
                     if talker_type == CHAT_TYPE_GROUP:
@@ -912,7 +916,7 @@ class GenerateModel(object):
                 if row[3]:
                     model.Name.Value = row[3]
                 if row[13]:
-                    model.Deleted.Value = self._convert_deleted_status(row[13])
+                    model.Deleted = self._convert_deleted_status(row[13])
                 models.append(model)
             row = cursor.fetchone()
         if cursor is not None:
@@ -940,7 +944,7 @@ class GenerateModel(object):
             if row[14]:
                 moment.Source.Value = row[14]
             if row[15]:
-                moment.Deleted.Value = self._convert_deleted_status(row[15])
+                moment.Deleted = self._convert_deleted_status(row[15])
             if row[0]:
                 moment.OwnerUserID.Value = row[0]
                 account_id = row[0]
@@ -961,7 +965,9 @@ class GenerateModel(object):
             if row[13]:
                 moment.Location.Value = self._get_location(row[13])
             if row[10]:
-                moment.TimeStamp.Value = self._get_timestamp(row[10])
+                ts = self._get_timestamp(row[10])
+                if ts:
+                    moment.TimeStamp.Value = ts
             if row[11]:
                 moment.Likes.AddRange(self._get_feed_likes(account_id, row[11]))
             if row[12]:
@@ -995,10 +1001,13 @@ class GenerateModel(object):
         return user
 
     def _get_timestamp(self, timestamp):
-        ts = TimeStamp.FromUnixTime(timestamp, False)
-        if not ts.IsValidForSmartphone():
-            ts = None
-        return ts
+        try:
+            ts = TimeStamp.FromUnixTime(timestamp, False)
+            if not ts.IsValidForSmartphone():
+                ts = None
+            return ts
+        except Exception as e:
+            return None
 
     def _get_uri(self, path):
         if path.startswith('http') or len(path) == 0:
@@ -1030,11 +1039,13 @@ class GenerateModel(object):
                 if row[0]:
                     like.User.Value = self._get_user_intro(account_id, row[0], row[1])
                 if row[2]:
-                    like.TimeStamp.Value = self._get_timestamp(row[2])
+                    ts = self._get_timestamp(row[2])
+                    if ts:
+                        like.TimeStamp.Value = ts
                 if row[3]:
                     like.Source.Value = row[3]
                 if row[4]:
-                    like.Deleted.Value = self._convert_deleted_status(row[4])
+                    like.Deleted = self._convert_deleted_status(row[4])
                 models.append(like)
 
                 row = cursor.fetchone()
@@ -1071,11 +1082,13 @@ class GenerateModel(object):
                 if row[4]:
                     comment.Content.Value = row[4]
                 if row[5]:
-                    comment.TimeStamp.Value = self._get_timestamp(row[5])
+                    ts = self._get_timestamp(row[5])
+                    if ts:
+                        comment.TimeStamp.Value = ts
                 if row[6]:
                     comment.Source.Value = row[6]
                 if row[7]:
-                    comment.Deleted.Value = self._convert_deleted_status(row[7])
+                    comment.Deleted = self._convert_deleted_status(row[7])
                 models.append(comment)
 
                 row = cursor.fetchone()
@@ -1108,11 +1121,13 @@ class GenerateModel(object):
                 if row[3]:
                     location.Position.Value.PositionAddress.Value = row[3]
                 if row[4]:
-                    location.TimeStamp.Value = self._get_timestamp(row[4])
+                    ts = self._get_timestamp(row[4])
+                    if ts:
+                        location.TimeStamp.Value = ts
                 if row[5]:
                     location.Source.Value = row[5]
                 if row[6]:
-                    location.Deleted.Value = self._convert_deleted_status(row[6])
+                    location.Deleted = self._convert_deleted_status(row[6])
 
             if cursor is not None:
                 cursor.close()
@@ -1134,6 +1149,10 @@ class GenerateModel(object):
             return Common.FriendType.MutualFollow
         elif friend_type == FRIEND_TYPE_RECENT:
             return Common.FriendType.Recent
+        elif friend_type == FRIEND_TYPE_SUBSCRIBE:
+            return Common.FriendType.Subscribe
+        elif friend_type == FRIEND_TYPE_STRANGER:
+            return Common.FriendType.Stranger
         else:
             return Common.FriendType.None
 
