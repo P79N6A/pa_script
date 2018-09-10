1c4b5f35fa343bc42d624038eb3dc28f07a9aa4f
YUGE <869468688@qq.com>
2018-09-10 10:40:02 +0800

    add

---
+++ android_soundrecord.py
@@ -0,0 +1,75 @@
+#coding=utf-8
+import os
+import PA_runtime
+import sqlite3
+from PA_runtime import *
+import logging 
+from model_soundrecord import *
+
+
+class SoundrecordParse(object):
+    def __init__(self, node, extractDeleted, extractSource):
+        self.node = node
+        self.extractDeleted = False
+        self.extractSource = extractSource
+        self.db = None
+        self.ms = MS()
+        self.cache_path = ds.OpenCachePath("SOUNDRECORD")
+        self.cachedb = self.cache_path + "\\soundrecord.db"
+        self.ms.db_create(self.cachedb)
+
+    def analyze_soundrecord_huawei(self):
+        records = Records()
+        try:
+            self.db = SQLiteParser.Database.FromNode(self.node)
+            if self.db is None:
+                return 
+            ts = SQLiteParser.TableSignature('normal_record_table')
+            for rec in self.db.ReadTableRecords(ts, self.extractDeleted, True):
+                records.record_id = rec['_id'].Value if '_id' in rec else None
+                records.record_name = rec['title'].Value if 'title' in rec else None
+                records.record_create = rec['date_added'].Value if 'date_added' in rec else None
+                if 'duration' in rec:
+                    records.record_duration = int((rec['duration'].Value)/1000) if rec['duration'].Value is not None else None
+                records.record_size = rec['file_size'].Value if 'file_size' in rec else None
+                records.record_url = rec['_data'].Value if '_data' in rec else None
+                self.ms.db_insert_table_records(records)
+            self.ms.db_commit()
+        except Exception as e:
+            logging.error(e)
+
+    def analyze_soundrecord_xiaomi(self):
+        records = Records()
+        try:
+            self.db = SQLiteParser.Database.FromNode(self.node)
+            if self.db is None:
+                return 
+            ts = SQLiteParser.TableSignature('records')
+            for rec in self.db.ReadTableRecords(ts, self.extractDeleted, True):
+                records.record_id = rec['_id'].Value if '_id' in rec else None
+                records.record_name = rec['file_name'].Value if 'file_name' in rec else None
+                records.record_create = rec['create_time'].Value if 'create_time' in rec else None
+                records.record_duration = rec['duration'].Value if 'duration' in rec else None
+                records.record_size = rec['file_size'].Value if 'file_size' in rec else None
+                records.record_url = rec['file_path'].Value if 'file_path' in rec else None
+                records.source = 'Android 语音备忘录'
+                self.ms.db_insert_table_records(records)
+            self.ms.db_commit()
+        except Exception as e:
+            logging.error(e)
+
+    def parse(self):
+        self.analyze_soundrecord_huawei()
+        self.analyze_soundrecord_xiaomi()
+        self.ms.db_close()
+        generate = Generate(self.cachedb)
+        models = generate.get_models()
+        return models
+
+def analyze_android_soundrecord(node, extractDeleted, extractSource):
+    pr = ParserResults()
+    pr.Models.AddRange(SoundrecordParse(node, extractDeleted, extractSource).parse())
+    return pr
+
+def execute(node, extractDeleted):
+    return analyze_android_soundrecord(node, extractDeleted, False)
---
+++ model_soundrecord.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+
+from PA_runtime import *
+import clr
+clr.AddReference('System.Core')
+clr.AddReference('System.Xml.Linq')
+del clr
+
+from System.IO import MemoryStream
+from System.Text import Encoding
+from System.Xml.Linq import *
+from System.Linq import Enumerable
+from System.Xml.XPath import Extensions as XPathExtensions
+
+import os
+import System
+import sqlite3
+import logging
+
+SQL_CREATE_TABLE_RECORDS = '''
+    CREATE TABLE IF NOT EXISTS records(
+        record_id INTEGER,
+        record_url TEXT,
+        record_name TEXT,
+        record_size INTEGER,
+        record_create INTEGER,
+        record_duration INTEGER,
+        source TEXT,
+        deleted INTEGER,
+        repeated INTEGER
+    )'''
+
+SQL_INSERT_TABLE_RECORDS = '''
+    INSERT INTO records(record_id, record_url, record_name, record_size, record_create,
+        record_duration, source, deleted, repeated)
+        values(?, ?, ?, ?, ?, ?, ?, ?, ?)
+    '''
+
+class MS(object):
+    def __init__(self):
+        self.db = None
+        self.cursor = None
+
+    def db_create(self,db_path):
+        if os.path.exists(db_path):
+            os.remove(db_path)
+        self.db = sqlite3.connect(db_path)
+        self.cursor = self.db.cursor()
+        self.db_create_tables()
+
+    def db_commit(self):
+        if self.db is not None:
+            self.db.commit()
+
+    def db_close(self):
+        if self.cursor is not None:
+            self.cursor.close()
+            self.cursor = None
+        if self.db is not None:
+            self.db.close()
+            self.db = None
+
+    def db_create_tables(self):
+        if self.cursor is not None:
+            self.cursor.execute(SQL_CREATE_TABLE_RECORDS)
+
+    def db_insert_table_records(self, Records):
+        if self.cursor is not None:
+            self.cursor.execute(SQL_INSERT_TABLE_RECORDS, Records.get_values())
+
+
+class Records(object):
+    def __init__(self):
+        self.record_id = None
+        self.record_url = None
+        self.record_name = None
+        self.record_size = None
+        self.record_create = None
+        self.record_duration = None
+        self.source = ''
+        self.deleted = 0
+        self.repeated = 0
+
+    def get_values(self):
+        return (self.record_id, self.record_url, self.record_name, self.record_size,
+            self.record_create, self.record_duration, self.source, self.deleted,
+            self.repeated)
+
+
+class Generate(object):
+
+    def __init__(self, db_cache):
+        self.db_cache = db_cache
+        self.db = None
+        self.cursor = None
+
+    def get_models(self):
+        models = []
+        self.db = sqlite3.connect(self.db_cache)
+        models.extend(self._get_model_records())
+        self.db.close()
+        self.db = None
+        return models
+
+    def _get_model_records(self):
+        model = []
+        self.cursor = self.db.cursor()
+        self.cursor.execute('select distinct * from records')
+        for row in self.cursor:
+            r = Generic.Recording()
+            if row[1] is not None:
+                r.URL.Value = row[1]
+            if row[2] is not None:
+                r.Title.Value = row[2]
+            if row[3] is not None:
+                r.ByteLength.Value = row[3]
+            if row[4] is not None:
+                r.TimeStamp.Value = TimeStamp.FromUnixTime(int(str(row[4])[0:10:1]), False) if len(str(row[4])) > 10 else TimeStamp.FromUnixTime(row[4], False) if len(str(row[4])) == 10 else TimeStamp.FromUnixTime(0, False)
+            if row[5] is not None:
+                hours = row[5]/3600
+                minutes = row[5]-hours*3600
+                seconds = row[5]-hours*3600-minutes*60
+                r.Duration.Value = System.TimeSpan(hours, minutes, seconds)
+            r.Type.Value = Generic.RecordingType.Audio
+        self.cursor.close()
+        self.cursor = None
+        model.append(r)
+        return model
