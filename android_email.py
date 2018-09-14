8db3233e1f65f0026fdf1343ada8948e6664d902
yly <yly@pwnzen.com>
2018-09-14 15:47:40 +0800

    add

---
+++ android_email.py
@@ -0,0 +1,351 @@
+# coding=utf-8
+import os
+import traceback
+import re
+
+import PA_runtime
+from PA_runtime import *
+from model_mails import *
+
+MESSAGE_STATUS_DEFAULT = 0
+MESSAGE_STATUS_UNSENT  = 1
+MESSAGE_STATUS_SENT    = 2
+MESSAGE_STATUS_UNREAD  = 3
+MESSAGE_STATUS_READ    = 4
+
+def execute(node, extract_deleted):
+    """ main """
+    return analyze_email(node, extract_deleted, extract_source=False)
+
+def exc():
+    pass
+    # traceback.print_exc()
+
+def analyze_email(node, extract_deleted, extract_source):
+    """ android 邮件 华为 """
+    pr = ParserResults()
+    res = EmailParser(node, extract_deleted, extract_source).parse()
+    pr.Models.AddRange(res)
+    return pr
+
+
+class EmailParser(object):
+    def __init__(self, node, extract_deleted, extract_source):
+
+        self.root = node
+        self.extract_deleted = extract_deleted
+        self.extract_source = extract_source
+
+        self.mm = MM()
+        self.cachepath = ds.OpenCachePath("AndroidEmail")
+        self.cachedb = self.cachepath + "\\AndroidEmail.db"
+        self.mm.db_create(self.cachedb)
+        self.mm.db_create_table()
+
+        self.accounts = {}
+        self.mail_folder = {}
+
+    def parse(self):
+        """ android 邮件        xiaomi
+        
+            (\data\com.android.email\databases\EmailProvider.db)
+            或者(\data\com.google.android.gm\databases)
+                1. Account          # 本人用户
+                2. Attachment       # 附件
+                3. Credential
+                4. HostAuth
+                5. Mailbox
+                6. Message          # main
+                7. Message_Deletes
+                8. Message_Updates
+                9. MessageMove
+                10. MessageStateChange
+                11. Policy
+                12. QuickResponse                  
+            (\data\com.android.email\databases\EmailProviderBody.db) 
+                - Body
+            (\data\com.android.email\databases\Contact.db)      华为没有     
+                - contact_table 
+                - nick_table
+            (\data\com.android.email\databases\EmailProvider.db)
+        """        
+        try:
+             self.parse_email_body(node=self.root.GetByPath("/EmailProviderBody.db"))
+             self.parse_email(node=self.root.GetByPath("/EmailProvider.db"))
+             self.parse_contact(node=self.root.GetByPath("/EmailProviderBody.db"))
+        except:
+            exc()
+        finally:
+            self.mm.db_close()
+
+        models = Generate(self.cachedb).get_models()
+        return models
+
+    def parse_email(self, node):
+        """ (/EmailProvider.db)
+            1. Account          # 本人用户
+            2. Attachment       # 附件
+            # 3. Credential
+            # 4. HostAuth
+            5. Mailbox
+            6. Message          # main
+            7. Message_Deletes
+            8. Message_Updates
+            9. MessageMove
+            10. MessageStateChange
+            11. Policy
+            12. QuickResponse  
+        """
+        self.db_email_provider = SQLiteParser.Database.FromNode(node)
+        self.source_emailprovider = node.AbsolutePath
+        
+        if self.db_email_provider is None:
+            return 
+        self.parse_email_tb_account()
+        self.parse_email_tb_attachment()
+        self.parse_email_tb_mailbox()
+        self.parse_email_tb_message()
+        
+    def parse_email_tb_account(self):
+        """ EmailProvider - Account """
+        for rec in self.my_read_table(self.db_email_provider, 'Account'):
+            if IsDBNull(rec['emailAddress'].Value) or not self._is_email_format(rec['emailAddress'].Value):
+                continue
+            account = Accounts()
+            account.accountId    = rec['_id'].Value
+            account.alias        = rec['displayName'].Value
+            account.accountEmail = rec['emailAddress'].Value
+            account.source       = self.source_emailprovider
+            self.accounts[account.accountId] = {
+                'email': account.accountEmail,
+                'name' : account.alias,
+            }
+            try:
+                self.mm.db_insert_table_account(account)
+            except:
+                exc()
+        self.mm.db_commit()
+
+    def parse_email_tb_attachment(self):
+        """ EmailProvider - Attachment
+                fileName         TEXT,
+                mimeType         TEXT,
+                size             INTEGER,
+                contentId        TEXT,
+                contentUri       TEXT,
+                messageKey       INTEGER,
+                location         TEXT,
+                encoding         TEXT,
+                content          TEXT,
+                flags            INTEGER,
+                content_bytes    BLOB,
+                accountKey       INTEGER,
+                uiState          INTEGER,
+                uiDestination    INTEGER,
+                uiDownloadedSize INTEGER,
+                cachedFile       TEXT        
+        """
+        for rec in self.my_read_table(self.db_email_provider, 'Attachment'):
+            if IsDBNull(rec['fileName'].Value) or IsDBNull(rec['size'].Value):
+                continue
+            attach = Attach()
+            attach.attachName   = rec['fileName'].Value
+            attach.attachDir    = rec['contentUri'].Value  # or location?
+            attach.attachType   = rec['mimeType'].Value
+            attach.downloadUtc  = rec['previewTime'].Value
+            attach.downloadSize = rec['size'].Value
+            attach.mailId       = rec['messageKey'].Value
+            attach.deleted      = rec['isDeleted'].Value
+            attach.source       = self.source_emailprovider
+            try:
+                self.mm.db_insert_table_attach(attach)
+            except:
+                exc()
+        self.mm.db_commit()
+
+    def parse_email_tb_mailbox(self):
+        """ EmailProvider - Mailbox """
+        REPLACE_NAME = {
+            'INBOX': '收件箱',
+            'Outbox': '发件箱',
+        }
+        for rec in self.my_read_table(self.db_email_provider, 'Mailbox', extract_deleted=False):
+            folder = MailFolder()
+            if IsDBNull(rec['displayName'].Value) or IsDBNull(rec['accountKey'].Value):
+                continue
+            folderName = rec['displayName'].Value.replace('\0','')
+            folder.folderName   = folderName if folderName not in REPLACE_NAME else REPLACE_NAME[folderName]
+            self.mail_folder[rec['_id'].Value] = folder.folderName
+            try:
+                folder.accountEmail = self.accounts[rec['accountKey'].Value]['email']
+                va = rec['accountKey'].Value
+            except:
+                exc()
+            folder.accountNick  = self.accounts[rec['accountKey'].Value]['name']
+            folder.source       = self.source_emailprovider
+        
+            try: 
+                self.mm.db_insert_table_mail_folder(folder)
+            except:
+                exc()
+        self.mm.db_commit()
+
+    def parse_email_tb_message(self):
+        """ EmailProvider - Message """
+        
+        SELECT_ATTACH_SQL = '''
+            select attachName, attachDir, downloadSize from attach where mailId=?
+        '''
+        for rec in self.my_read_table(self.db_email_provider, 'Message'):
+
+            mail = Mails()
+            if IsDBNull(rec['subject'].Value) or IsDBNull(rec['accountKey'].Value):
+                continue
+            mail.mailId      = rec['_id'].Value
+            mail.mail_folder = self.mail_folder.get(rec['mailboxKey'].Value, None)
+            mail.subject     = rec['subject'].Value
+            mail.abstract    = rec['snippet'].Value
+            mail.accountId   = rec['accountKey'].Value
+            mail.fromEmail   = rec['fromList'].Value        # TODO 类型 提取 邮箱地址
+            mail.tos         = rec['toList'].Value
+            mail.cc          = rec['ccList'].Value
+            mail.bcc         = rec['bccList'].Value
+            # mail.isForward   = rec['isForwarded'].Value
+            mail.isRead      = rec['flagRead'].Value
+            mail.receiveUtc  = rec['timeStamp'].Value
+            try:
+                mail.size    = rec['messageSize'].Value    # 此字段华为没有
+            except:
+                pass
+            # mail.content     = rec['htmlContent'].Value
+            mail.source      = self.source_emailprovider
+
+            if self.mail_folder.get(rec['mailboxKey'].Value, None) == '已发送':
+                mail.sendStatus = 1
+            elif self.mail_folder.get(rec['mailboxKey'].Value, None) == '草稿箱':
+                mail.sendStatus = 0
+
+            # 附件
+            if rec['flagAttachment'].Value == 1:
+                self.mm.cursor.execute(SELECT_ATTACH_SQL, (mail.mailId,))
+                rows = self.mm.cursor.fetchall() # list, None 值以 '' 代替
+                mail.attachName   = ','.join(map(lambda x: x[0] if x[0] is not None else '', rows))
+                mail.attachDir    = ','.join(map(lambda x: x[1] if x[1] is not None else '', rows))
+                mail.downloadSize = ','.join(map(lambda x: x[2] if x[2] is not None else '', rows))
+
+            try:
+                self.mm.db_insert_table_mails(mail)
+            except:
+                exc()
+        self.mm.db_commit()
+
+    def parse_email_body(self, node):
+        """
+        /EmailProviderBody.db
+        """
+        self.db_email_provider_body = SQLiteParser.Database.FromNode(node)
+
+        SQL_UPDATE_EMAIL_CONTENT = '''
+            update mails set content=? where mailId=?
+        '''
+        def handle_html(h):
+            # return h
+            # return h if h.count('"') % 2 == 0 else h+'"'
+            return h.replace('\0','')      
+
+        try:
+            for rec in self.my_read_table(self.db_email_provider_body, 'Body'):
+                if IsDBNull(rec['htmlContent'].Value) or IsDBNull(rec['messageKey'].Value):
+                    continue
+                mailId = rec['messageKey'].Value
+                mailContent = rec['htmlContent'].Value
+                try:
+                    self.mm.cursor.execute(SQL_UPDATE_EMAIL_CONTENT, (mailContent, mailId))
+                except:
+                    exc()
+            self.mm.db_commit()
+        except:
+            exc()
+
+    def parse_contact(self, node):
+        """
+        华为没有此 db
+            /Contact.db
+        """
+        try:        
+            self.db_contact = SQLiteParser.Database.FromNode(node)
+            self.source_contact = node.AbsolutePath
+
+            SQL_UPDATE_EMAIL_CONTENT = '''
+                update mails set content=? where mailId=?
+            '''
+            for rec in self.my_read_table(self.db_contact, 'contact_table'):
+                if IsDBNull(rec['name'].Value) or IsDBNull(rec['email'].Value):
+                    continue
+                contact = Contact()
+                contact.contactName  = rec['name'].Value
+                contact.contactEmail = rec['email'].Value
+                contact.source       = node.AbsolutePath
+                try:
+                    self.mm.db_insert_table_contact(contact)
+                except:
+                    exc()
+            self.mm.db_commit()
+        except:
+            pass
+            # exc()
+            
+    def my_read_table(self, db, table_name, extract_deleted=None):
+        """ 
+        读取手机数据库, 多数据库模式
+        :type db: SQLiteParser.Database.FromNode(node)tr
+        :type table_name: str 
+        :rtype: db.ReadTableRecords()
+        """
+        if extract_deleted == None:
+            extract_deleted = self.extract_deleted
+        if db is None:
+            return
+        tb = SQLiteParser.TableSignature(table_name)
+        return db.ReadTableRecords(tb, extract_deleted, True)
+
+    @staticmethod
+    def _timestamp_long2int(_long):
+        """ long 13 位 => int 10 位 """
+        if type(_long)==long:
+            return int(_long * 0.001)
+        return _long
+
+    @staticmethod
+    def _convert_email(eml):
+        """
+        pangu_x01 <pangu_x01@163.com>, pangu_x02 <pangu_x02@163.com>
+        pangu_x01@163.com pangu_x01 pangu_x02@163.com pangu_x02
+        """
+        if not eml:
+            return None
+        try:
+            res = ''
+            for i in eml.split(', '):
+                name  = i.split(' ')[0]
+                email = i.split(' ')[1][1:-1]
+                res += ' ' + email + ' ' + name
+            return res.lstrip()
+        except:
+            return None
+
+    @staticmethod
+    def _is_email_format(rec):
+        """ 匹配邮箱地址 """
+        try:
+            if IsDBNull(rec) or len(rec.strip()) < 5:
+                return False
+            reg_str = r'^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$'
+            match_obj = re.match(reg_str, rec)
+            if match_obj is None:
+                return False      
+            return True      
+        except:
+            return False
+
+        
---
+++ android_sms.py
@@ -0,0 +1,194 @@
+# coding=utf-8
+import os
+import re
+
+import PA_runtime
+from PA_runtime import *
+from model_sms import *
+
+SMS_TYPE_ALL    = 0
+SMS_TYPE_INBOX  = 1
+SMS_TYPE_SENT   = 2
+SMS_TYPE_DRAFT  = 3
+SMS_TYPE_OUTBOX = 4
+SMS_TYPE_FAILED = 5
+SMS_TYPE_QUEUED = 6
+
+
+def execute(node, extract_deleted):
+    """ main """
+    return analyze_sms(node, extract_deleted, extract_source=False)
+
+def analyze_sms(node, extract_deleted, extract_source):
+    """
+        android 小米 短信 (user_de/0/com.android.providers.telephony/databases/mmssms.db)
+    """
+    pr = ParserResults()
+    res = SMSParser(node, extract_deleted, extract_source).parse()
+    pr.Models.AddRange(res)
+    return pr
+
+
+class SMSParser(object):
+    """  """
+    def __init__(self, node, extract_deleted, extract_source):
+
+        self.root = node
+        self.extract_deleted = extract_deleted
+        self.extract_source = extract_source
+
+        self.sms = Model_SMS()
+        self.cachepath = ds.OpenCachePath("AndroidSMS")
+        self.cachedb = self.cachepath + "\\AndroidSMS.db"
+        self.sms.db_create(self.cachedb)
+        self.sms.db_create_table()
+        
+        self.unread_msgs = []
+
+    def parse(self):
+        node = self.root.GetByPath("/mmssms.db")
+        self.db = SQLiteParser.Database.FromNode(node)
+        if self.db is None:
+            return
+        self.source_mmmssms_db = node.AbsolutePath
+
+        self.parse_main()
+
+        self.sms.db_close()
+        models = GenerateModel(self.cachedb).get_models()
+        return models
+
+    def parse_main(self):
+        """
+        sms
+            _id                INTEGER PRIMARY KEY,
+            thread_id          INTEGER,
+            address            TEXT,
+            person             INTEGER, 联系人（模块）列表里的序号，陌生人为null
+            date               INTEGER, 毫秒
+            date_sent          INTEGER DEFAULT 0,
+            read               INTEGER DEFAULT 0,
+            status             INTEGER DEFAULT -1, -1 默认值， 0-complete ， 64-pending ， 128-failed
+            type               INTEGER,
+                                    ALL=0;INBOX=1;SENT=2;DRAFT=3;OUTBOX=4;FAILED=5;QUEUED=6;
+            subject            TEXT,
+            body               TEXT,
+            seen               INTEGER DEFAULT 0,
+            timed              INTEGER DEFAULT 0,
+            deleted            INTEGER DEFAULT 0,
+            sync_state         INTEGER DEFAULT 0,
+            marker             INTEGER DEFAULT 0,
+            source             TEXT,
+            bind_id            INTEGER DEFAULT 0,
+            mx_status          INTEGER DEFAULT 0,
+            mx_id              INTEGER,
+            out_time           INTEGER DEFAULT 0,
+            account            TEXT,
+            sim_id             INTEGER DEFAULT 0,
+            block_type         INTEGER DEFAULT 0,
+            advanced_seen      INTEGER DEFAULT 0,
+            b2c_ttl            INTEGER DEFAULT 0,
+            b2c_numbers        TEXT,
+            fake_cell_type     INTEGER DEFAULT 0,
+            url_risky_type     INTEGER DEFAULT 0,
+            creator            TEXT,
+            favorite_date      INTEGER DEFAULT 0
+        """
+        self.parse_sim_cards()
+        self.parse_sms()
+        # self.parse_mms()
+        
+    def parse_sim_cards(self):
+        """ 
+            sms - 短信
+        """
+        try:
+            for rec in self.my_read_table('sim_cards'):
+                if IsDBNull(rec['number'].Value):
+                    continue
+                sim = Sim_cards()
+                sim.sim_id       = rec['sim_id'].Value
+                sim.number       = rec['number'].Value
+                sim.sync_enabled = rec['sync_enabled'].Value
+                sim.source       = self.source_mmmssms_db
+                try:
+                    self.sms.db_insert_table_sim_cards(sim)
+                except:
+                    exc()
+            try:
+                self.sms.db_commit()
+            except:
+                pass
+        except:
+            pass
+
+    def parse_sms(self):
+        """ 
+            sms - 短信
+        """
+        for rec in self.my_read_table(table_name='sms'):
+            if IsDBNull(rec['body'].Value):
+                continue
+            sms = SMS()
+            sms.sms_id             = rec['_id'].Value
+            sms.sender_phonenumber = rec['address'].Value
+            sms.sms_or_mms         = 'sms'
+            sms.type               = rec['type'].Value    # SMS_TYPE
+            sms.subject            = rec['subject'].Value # decode or what
+            sms.body               = rec['body'].Value.replace('\0', '')
+            sms.send_time          = rec['date_sent'].Value    # or date_sent?
+            sms.deliverd           = rec['date'].Value
+            sms.is_sender          = 1 if sms.type == SMS_TYPE_SENT else 0
+            try: # 华为没有的字段
+                sms.sim_id         = rec['sim_id'].Value
+                sms.deleted        = rec['deleted'].Value
+            except:
+                pass    
+            sms.source             = self.source_mmmssms_db
+            try:
+                self.sms.db_insert_table_sms(sms)
+            except:
+                exc()
+        try:
+            self.sms.db_commit()
+        except:
+            exc()
+
+    def parse_mms(self):
+        """ 
+            pdu - 彩信
+        """
+        for rec in self.my_read_table(table_name='pdu'):
+            if IsDBNull(rec['address'].Value) or IsDBNull(rec['body'].Value):
+                continue
+            sms = Message()
+            sms.sms_or_mms         = 'sms'
+            sms.sender_phonenumber = rec['address'].Value
+            sms.sms_id             = rec['_id'].Value
+            sms.subject            = rec['subject'].Value # decode or what
+            sms.body               = rec['body'].Value
+            sms.send_time          = self._long2int_timestamp(rec['date'].Value)
+            sms.deliverd           = self._long2int_timestamp(rec['date'].Value)    
+            sms.status             = rec['type'].Value    # SMS_TYPE
+            sms.is_sender          = 1 if sms.status == SMS_TYPE_SENT else 0
+            sms.deleted            = rec['deleted'].Value
+            sms.source             = self.source_mmmssms_db
+            try:
+                self.sms.db_insert_table_sms(sms)
+            except:
+                exc()
+        try:
+            self.sms.db_commit()
+        except:
+            exc()            
+
+    def my_read_table(self, table_name):
+        """
+            读取手机数据库, 单数据库模式
+        :type table_name: str
+        :rtype: db.ReadTableRecords()
+        """
+        if self.db is None:
+            return
+        tb = SQLiteParser.TableSignature(table_name)
+        return self.db.ReadTableRecords(tb, self.extract_deleted, True)
