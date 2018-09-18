f20bf1441e961ebebc6529e1011a186099510a59
yly <yly@pwnzen.com>
2018-09-18 19:08:37 +0800

    fix

--- android_neteasemail.py
+++ android_neteasemail.py
@@ -36,9 +36,8 @@ def analyze_neteasemail(node, extract_deleted, extract_source):
     """
     pr = ParserResults()
     res = NeteaseMailParser(node, extract_deleted, extract_source).parse()
-    pr.Build('网易邮箱大师')
     pr.Models.AddRange(res)
-
+    pr.Build('网易邮箱大师')
     return pr
 
 
