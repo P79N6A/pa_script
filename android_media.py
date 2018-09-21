00db39e5ed80206a58da0352c0c76be2ccb17743
YUGE <869468688@qq.com>
2018-09-21 14:49:17 +0800

    modified

--- android_media.py
+++ android_media.py
@@ -1,15 +1,11 @@
-#coding=utf-8
+# -*- coding: utf-8 -*-
 import os
 import PA_runtime
 import sqlite3
 from PA_runtime import *
-import logging 
-import clr
-try:
-    clr.AddReference('model_meida')
-except:
-    pass
-del clr
+import threading
+import logging, traceback
+SafeLoadAssembly('model_media')
 from model_media import *
 
 
@@ -26,71 +22,113 @@ class MediaParse(object):
         self.mm.db_create(self.db_cache)
 
     def analyze_media(self):
-        media = Media()
         for i, node in enumerate(self.nodes):
             try:
-                self.db = SQLiteParser.Database.FromNode(node)
+                self.db = SQLiteParser.Database.FromNode(node, canceller)
                 if self.db is None:
                     return 
                 ts = SQLiteParser.TableSignature('files')
+                media = Media()
                 for rec in self.db.ReadTableRecords(ts, self.extractDeleted, True):
-                    media.id = rec['_id'].Value if '_id' in rec and not rec['_id'].IsDBNull else None
-                    media.url = rec['_data'].Value if '_data' in rec and not rec['_data'].IsDBNull else None
-                    media.size = rec['_size'].Value if '_size' in rec and not rec['_size'].IsDBNull else None
-                    media.parent = rec['parent'].Value if 'parent' in rec and not rec['parent'].IsDBNull else None
-                    media.add_date = rec['date_added'].Value if 'date_added' in rec and not rec['date_added'].IsDBNull else None
-                    media.modify_date = rec['date_modified'].Value if 'date_modified' in rec and not rec['date_modified'].IsDBNull else None
-                    media.mime_type = rec['mime_type'].Value if 'mime_type' in rec and not rec['mime_type'].IsDBNull else None
-                    media.title = rec['title'].Value if 'title' in rec and not rec['title'].IsDBNull else None
-                    media.display_name = rec['_display_name'].Value if '_display_name' in rec and not rec['_display_name'].IsDBNull else None
-                    media.latitude = rec['latitude'].Value if 'latitude' in rec and not rec['latitude'].IsDBNull else None
-                    media.longitude = rec['longitude'].Value if 'longitude' in rec and not rec['longitude'].IsDBNull else None
-                    media.datetaken = rec['datetaken'].Value if 'datetaken' in rec and not rec['datetaken'].IsDBNull else None
-                    media.bucket_display_name = rec['bucket_display_name'].Value if 'bucket_display_name' in rec and not rec['bucket_display_name'].IsDBNull else None
-                    media.year = rec['year'].Value if 'year' in rec and not rec['year'].IsDBNull else None
-                    media.album_artist = rec['album_artist'].Value if 'album_artist' in rec and not rec['album_artist'].IsDBNull else None
-                    media.duration = rec['duration'].Value if 'duration' in rec and not rec['duration'].IsDBNull else None
-                    media.artist = rec['artist'].Value if 'artist' in rec and not rec['artist'].IsDBNull else None
-                    media.album = rec['album'].Value if 'album' in rec and not rec['album'].IsDBNull else None
+                    canceller.ThrowIfCancellationRequested()
+                    media.id = rec['_id'].Value
+                    media.url = rec['_data'].Value
+                    media.size = rec['_size'].Value
+                    media.parent = rec['parent'].Value
+                    media.add_date = rec['date_added'].Value
+                    media.modify_date = rec['date_modified'].Value
+                    media.mime_type = rec['mime_type'].Value
+                    media.title = rec['title'].Value
+                    media.display_name = rec['_display_name'].Value
+                    media.latitude = rec['latitude'].Value
+                    media.longitude = rec['longitude'].Value
+                    media.datetaken = rec['datetaken'].Value
+                    media.bucket_display_name = rec['bucket_display_name'].Value
+                    media.year = rec['year'].Value
+                    media.album_artist = rec['album_artist'].Value
+                    media.duration = rec['duration'].Value
+                    media.artist = rec['artist'].Value
+                    media.album = rec['album'].Value
                     media.location = 'internal' if i==0 else 'external'
-                    media.source = 'android多媒体'
+                    media.source = node.AbsolutePath
                     self.mm.db_insert_table_media(media)
                 self.mm.db_commit()
-            except Exception as e:
-                logging.error(e)
+                media = Media()
+                for rec in self.db.ReadTableDeletedRecords(ts, False):
+                    canceller.ThrowIfCancellationRequested()
+                    media.id = rec['_id'].Value
+                    media.url = rec['_data'].Value
+                    media.size = rec['_size'].Value
+                    media.parent = rec['parent'].Value
+                    media.add_date = rec['date_added'].Value
+                    media.modify_date = rec['date_modified'].Value
+                    media.mime_type = repr(rec['mime_type'].Value)
+                    media.title = rec['title'].Value
+                    media.display_name = rec['_display_name'].Value
+                    media.latitude = rec['latitude'].Value
+                    media.longitude = rec['longitude'].Value
+                    media.datetaken = rec['datetaken'].Value
+                    media.bucket_display_name = rec['bucket_display_name'].Value
+                    media.year = rec['year'].Value
+                    media.album_artist = rec['album_artist'].Value
+                    media.duration = rec['duration'].Value
+                    media.artist = rec['artist'].Value
+                    media.album = rec['album'].Value
+                    media.location = 'internal' if i==0 else 'external'
+                    media.source = node.AbsolutePath
+                    media.deleted = 1
+                    self.mm.db_insert_table_media(media)
+                self.mm.db_commit()
+            except:
+                traceback.print_exc()
 
     def analyze_thumbnails(self):
-        thumbnails = Thumbnails()
         for i, node in enumerate(self.nodes):
             try:
-                self.db = SQLiteParser.Database.FromNode(node)
+                self.db = SQLiteParser.Database.FromNode(node, canceller)
                 if self.db is None:
                     return 
                 ts = SQLiteParser.TableSignature('thumbnails')
+                thumbnails = Thumbnails()
                 for rec in self.db.ReadTableRecords(ts, self.extractDeleted, True):
-                    thumbnails.id = rec['_id'].Value if '_id' in rec else None
-                    thumbnails.url = rec['_data'].Value if '_data' in rec else None
-                    thumbnails.image_id = rec['image_id'].Value if 'image_id' in rec else None
-                    thumbnails.width = rec['width'].Value if 'width' in rec else None
-                    thumbnails.height = rec['height'].Value if 'height' in rec else None
+                    canceller.ThrowIfCancellationRequested()
+                    thumbnails.id = rec['_id'].Value
+                    thumbnails.url = rec['_data'].Value
+                    thumbnails.image_id = rec['image_id'].Value
+                    thumbnails.width = rec['width'].Value
+                    thumbnails.height = rec['height'].Value
                     thumbnails.location = 'internal' if i==0 else 'external'
-                    thumbnails.source = 'android多媒体'
+                    thumbnails.source = node.AbsolutePath
                     self.mm.db_insert_table_thumbnails(thumbnails)
                 self.mm.db_commit()
-            except Exception as e:
-                logging.error(e)
+                thumbnails = Thumbnails()
+                for rec in self.db.ReadTableDeletedRecords(ts, False):
+                    canceller.ThrowIfCancellationRequested()
+                    thumbnails.id = rec['_id'].Value
+                    thumbnails.url = rec['_data'].Value
+                    thumbnails.image_id = rec['image_id'].Value
+                    thumbnails.width = rec['width'].Value
+                    thumbnails.height = rec['height'].Value
+                    thumbnails.location = 'internal' if i==0 else 'external'
+                    thumbnails.source = node.AbsolutePath
+                    thumbnails.deleted = 1
+                    self.mm.db_insert_table_thumbnails(thumbnails)
+                self.mm.db_commit()
+            except:
+                traceback.print_exc()
 
     def parse(self):
-        self.analyze_media()
         self.analyze_thumbnails()
+        self.analyze_media()
         self.mm.db_close()
-        generate = Generate(self.db_cache)
+        generate = Generate(self.db_cache, self.node)
         models = generate.get_models()
         return models
 
 def analyze_android_media(node, extractDeleted, extractSource):
     pr = ParserResults()
     pr.Models.AddRange(MediaParse(node, extractDeleted, extractSource).parse())
+    pr.Build('Media')
     return pr
 
 def execute(node, extractDeleted):
--- model_media.py
+++ model_media.py
@@ -15,7 +15,7 @@ from System.Xml.XPath import Extensions as XPathExtensions
 import os
 import System
 import sqlite3
-import logging
+import re
 
 SQL_CREATE_TABLE_MEDIA = '''
     CREATE TABLE IF NOT EXISTS media(
@@ -133,6 +133,15 @@ class Media(object):
         self.deleted = 0
         self.repeated = 0
 
+    def __setattr__(self, name, value):
+        if not IsDBNull(value):
+            if isinstance(value, str):
+                self.__dict__[name] = re.compile(u"[\\x00-\\x08\\x0b-\\x0c\\x0e-\\x1f]").sub('', value)
+            else:
+                self.__dict__[name] = value
+        else:
+            self.__dict__[name] = None
+
     def get_values(self):
         return (self.id, self.url, self.size, self.parent, self.add_date, self.modify_date, self.mime_type, self.title,
             self.display_name, self.latitude, self.longitude, self.datetaken, self.bucket_display_name, self.year,
@@ -151,26 +160,155 @@ class Thumbnails(object):
         self.deleted = 0
         self.repeated = 0
 
+    def __setattr__(self, name, value):
+        if not IsDBNull(value):
+            if isinstance(value, str):
+                self.__dict__[name] = re.compile(u"[\\x00-\\x08\\x0b-\\x0c\\x0e-\\x1f]").sub('', value)
+            else:
+                self.__dict__[name] = value
+        else:
+            self.__dict__[name] = None
+
     def get_values(self):
         return (self.id, self.url, self.image_id, self.width, self.height, self.location, self.source, self.deleted, self.repeated)
 
 
 class Generate(object):
 
-    def __init__(self, db_cache):
+    def __init__(self, db_cache, node):
         self.db_cache = db_cache
         self.db = None
         self.cursor = None
+        self.node = node
 
     def get_models(self):
         models = []
         self.db = sqlite3.connect(self.db_cache)
         models.extend(self._get_model_media())
+        models.extend(self._get_model_thumbnail())
         self.db.close()
         self.db = None
         return models
 
     def _get_model_media(self):
         model = []
-        
+        try:
+            cursor = self.db.cursor()
+            cursor.execute('select distinct * from media')
+            for row in cursor:
+                media = Common.Media()
+                if row[17] is not None and row[17] is not '':
+                    media.Album.Value = row[17]
+                coordinate = Locations.Coordinate()
+                if row[9] is not None and row[9] is not '':
+                    coordinate.Latitude.Value = row[9]
+                if row[10] is not None and row[10] is not '':
+                    coordinate.Longitude.Value = row[10]
+                media.Position.Value = coordinate
+                if row[5] is not None and row[5] is not '':
+                    media.ModifyDate.Value = self._get_timestamp(row[5])
+                if row[0] is not None and row[0] is not '':
+                    media.ID.Value = str(row[0])
+                if row[14] is not None and row[14] is not '':
+                    media.AlbumArtist.Value = row[14]
+                if row[12] is not None and row[12] is not '':
+                    media.BucketDisplayName.Value = row[12]
+                if row[1] is not None and row[1] is not '':
+                    media.Url.Value = self._transToAbsolutePath(row[1])
+                if row[11] is not None and row[11] is not '':
+                    media.DateTaken.Value = self._get_timestamp(row[11])
+                if row[15] is not None and row[15] is not '':
+                    hours = row[15]/3600
+                    minutes = row[15]-hours*3600
+                    seconds = row[15]-hours*3600-minutes*60
+                    media.Duration.Value = System.TimeSpan(hours, minutes, seconds)
+                if row[7] is not None and row[7] is not '':
+                    media.Title.Value = row[7]
+                if row[2] is not None and row[2] is not '':
+                    media.Size.Value = row[2]
+                if row[6] is not None and row[6] is not '':
+                    media.MimeType.Value = row[6]
+                if row[8] is not None and row[8] is not '':
+                    media.DisplayName.Value = row[8]
+                if row[4] is not None and row[4] is not '':
+                    media.AddDate.Value = self._get_timestamp(row[4])
+                if row[3] is not None and row[3] is not '':
+                    media.Parent.Value = row[3]
+                if row[16] is not None and row[16] is not '':
+                    media.Artist.Value = row[16]
+                if row[18] is not None and row[18] is not '':
+                    media.Location.Value = row[18]
+                if row[19] is not None and row[19] is not '':
+                    media.SourceFile.Value = self._get_source_file(str(row[19]))
+                model.append(media)
+        except:
+                traceback.print_exc()
         return model
+
+    def _get_model_thumbnail(self):
+        model = []
+        try:
+            cursor = self.db.cursor()
+            cursor.execute('select distinct * from thumbnails')
+            for row in cursor:
+                thumbnail = Common.Thumbnail()
+                if row[0] is not None and row[0] is not '':
+                    thumbnail.MediaID.Value = str(row[0])
+                if row[2] is not None and row[2] is not '':
+                    thumbnail.ID.Value = str(row[2])
+                if row[1] is not None and row[1] is not '':
+                    thumbnail.Url.Value = self._transToAbsolutePath(row[1])
+                if row[3] is not None and row[3] is not '':
+                    thumbnail.Width.Value = row[3]
+                if row[5] is not None and row[5] is not '':
+                    thumbnail.Location.Value = row[5]
+                if row[4] is not None and row[4] is not '':
+                    thumbnail.Height.Value = row[4]
+                if row[6] is not None and row[6] is not '':
+                    thumbnail.SourceFile.Value = self._get_source_file(str(row[6]))
+        except:
+                traceback.print_exc()
+        return model
+
+    def _get_source_file(self, source_file):
+        if isinstance(source_file, str):
+            return source_file.replace('/', '\\')
+        return source_file
+
+    def _get_timestamp(self, timestamp):
+        try:
+            if isinstance(timestamp, (long, float, str)) and len(str(timestamp)) > 10:
+                timestamp = int(str(timestamp)[:10])
+            if isinstance(timestamp, int) and len(str(timestamp)) == 10:
+                ts = TimeStamp.FromUnixTime(timestamp, False)
+                if not ts.IsValidForSmartphone():
+                    ts = TimeStamp.FromUnixTime(0, False)
+                return ts
+        except:
+            return TimeStamp.FromUnixTime(0, False)
+
+    def _transToAbsolutePath(self, dir):
+        fs = self.node.FileSystem
+        try:
+            if re.match(r'^/storage/emulated/0', dir) is not None:
+                subdir = dir.replace('/storage/emulated/0', '')
+                fileNode = list(fs.Search(subdir + '$'))
+                return fileNode[0].AbsolutePath
+            elif  re.match(r'^/data/user/0', dir) is not None:
+                subdir = dir.replace('/data/user/0', '')
+                fileNode = list(fs.Search(subdir + '$'))
+                return fileNode[0].AbsolutePath
+            elif re.match(r'^/storage/0000-0000', dir) is not None:
+                fileNode = fs.Search(subdir + '$')
+                fileNode = list(fs.Search(subdir + '$'))
+                return fileNode[0].AbsolutePath
+            elif re.match(r'^/$', dir) is not None:
+                return fs.AbsolutePath
+            elif '*' not in dir:
+                fileNode = fs.Search(dir+ '$')
+                fileNode = list(fs.Search(subdir + '$'))
+                return fileNode[0].AbsolutePath
+            else:
+                return ''
+        except:
+            pass
