9aa6c9c88b4132d530995951f92623aa9b1c0a4d
YUGE <869468688@qq.com>
2018-09-26 17:51:52 +0800

    modified

--- android_qqbrowser.py
+++ android_qqbrowser.py
@@ -12,6 +12,7 @@ except:
 del clr
 from model_browser import *
 
+import traceback
 
 class QQBrowserParse(object):
     def __init__(self, node, extractDeleted, extractSource):
@@ -42,7 +43,8 @@ class QQBrowserParse(object):
                 for rec in self.db.ReadTableRecords(ts, self.extractDeleted, True):
                     canceller.ThrowIfCancellationRequested()
                     bookmark.id = rec['_id'].Value if '_id' in rec else None
-                    bookmark.time = self._timeHandler(rec['created'].Value) if 'created' in rec else None
+                    if not IsDBNull(rec['created'].Value):
+                        bookmark.time = self._timeHandler(rec['created'].Value) if 'created' in rec else None
                     bookmark.title = rec['title'].Value if 'title' in rec and rec['title'].Value is not '' else None
                     bookmark.url = rec['url'].Value if 'url' in rec and rec['url'].Value is not '' else None
                     users = re.findall(r'^.*/(.+).db', node.AbsolutePath)
@@ -53,7 +55,8 @@ class QQBrowserParse(object):
                 for row in self.db.ReadTableDeletedRecords(ts, False):
                     canceller.ThrowIfCancellationRequested()
                     bookmark.id = row['_id'].Value if '_id' in row else None
-                    bookmark.time = self._timeHandler(row['created'].Value) if 'created' in row else None
+                    if not IsDBNull(rec['created'].Value):
+                        bookmark.time = self._timeHandler(row['created'].Value) if 'created' in row else None
                     bookmark.title = pattern.sub('', row['title'].Value) if 'title' in row and row['title'].Value is not '' else None
                     bookmark.url = pattern.sub('', row['url'].Value) if 'url' in row and row['url'].Value is not '' else None
                     users = re.findall(r'^.*/(.+).db', node.AbsolutePath)
@@ -63,6 +66,7 @@ class QQBrowserParse(object):
                     self.mb.db_insert_table_bookmarks(bookmark)
                 self.mb.db_commit()
         except Exception as e:
+            traceback.print_exc()
             print(e)
 
     def analyze_browserecords(self):
@@ -79,20 +83,23 @@ class QQBrowserParse(object):
                 record.id = rec['ID'].Value if 'ID' in rec else None
                 record.name = rec['NAME'].Value if 'NAME' in rec and rec['NAME'].Value is not '' else None
                 record.url = rec['URL'].Value if 'URL' in rec and rec['URL'].Value is not '' else None
-                record.datetime = self._timeHandler(rec['DATETIME'].Value) if 'DATETIME' in rec else None
+                if not IsDBNull(rec['DATETIME'].Value):
+                    record.datetime = self._timeHandler(rec['DATETIME'].Value) if 'DATETIME' in rec else None
                 self.source = node.AbsolutePath
             self.mb.db_commit()
             for row in self.db.ReadTableDeletedRecords(ts, False):
                 canceller.ThrowIfCancellationRequested()
                 record.id = row['ID'].Value if 'ID' in row else None
-                record.name = pattern.sub('', row['NAME'].Value) if 'NAME' in row and row['NAME'].Value is not '' else None
-                record.url = pattern.sub('', row['URL'].Value) if 'URL' in row and row['URL'].Value is not '' else None
-                record.datetime = self._timeHandler(row['DATETIME'].Value) if 'DATETIME' in row else None
+                record.name = row['NAME'].Value if 'NAME' in row and row['NAME'].Value is not '' else None
+                record.url = row['URL'].Value if 'URL' in row and row['URL'].Value is not '' else None
+                if not IsDBNull(rec['DATETIME'].Value):
+                    record.datetime = self._timeHandler(row['DATETIME'].Value) if 'DATETIME' in row else None
                 record.source = node.AbsolutePath
                 record.deleted = 1
                 self.mb.db_insert_table_browserecords(record)
             self.mb.db_commit()
         except Exception as e:
+            traceback.print_exc()
             print(e)
 
     def analyze_downloadfiles(self):
@@ -109,10 +116,13 @@ class QQBrowserParse(object):
                 downloadfile.id = rec['id'].Value if 'id' in rec else None
                 downloadfile.url = rec['url'].Value if 'url' in rec and rec['url'].Value is not '' else None
                 downloadfile.filename = rec['filename'].Value if 'filename' in rec and rec['filename'].Value is not '' else None
-                downloadfile.filefolderpath = self._transToAbsolutePath(rec['filefolderpath'].Value) if 'filefolderpath' in rec and rec['filefolderpath'].Value is not '' else None
+                if not IsDBNull(rec['filefolderpath'].Value):
+                    downloadfile.filefolderpath = self._transToAbsolutePath(rec['filefolderpath'].Value) if 'filefolderpath' in rec and rec['filefolderpath'].Value is not '' else None
                 downloadfile.totalsize = rec['totalsize'].Value if 'totalsize' in rec else None
-                downloadfile.createdate = self._timeHandler(rec['createdate'].Value) if 'createdate' in rec else None
-                downloadfile.donedate = self._timeHandler(rec['donedate'].Value) if 'donedate' in rec else None
+                if not IsDBNull(rec['createdate'].Value):
+                    downloadfile.createdate = self._timeHandler(rec['createdate'].Value) if 'createdate' in rec else None
+                if not IsDBNull(rec['donedate'].Value):
+                    downloadfile.donedate = self._timeHandler(rec['donedate'].Value) if 'donedate' in rec else None
                 downloadfile.costtime = rec['costtime'].Value if 'costtime' in rec else None
                 downloadfile.source = node.AbsolutePath
                 self.mb.db_insert_table_downloadfiles(downloadfile)
@@ -120,18 +130,22 @@ class QQBrowserParse(object):
             for row in self.db.ReadTableDeletedRecords(ts, False):
                 canceller.ThrowIfCancellationRequested()
                 downloadfile.id = row['id'].Value if 'id' in row else None
-                downloadfile.url = pattern.sub('', row['url'].Value) if 'url' in row and row['url'].Value is not '' else None
-                downloadfile.filename = pattern.sub('', row['filename'].Value) if 'filename' in row and row['filename'].Value is not '' else None
-                downloadfile.filefolderpath = self._transToAbsolutePath(pattern.sub(' ', row['filefolderpath'].Value)) if 'filefolderpath' in row and row['filefolderpath'].Value is not '' else None
+                downloadfile.url = row['url'].Value if 'url' in row and row['url'].Value is not '' else None
+                downloadfile.filename = row['filename'].Value if 'filename' in row and row['filename'].Value is not '' else None
+                if not IsDBNull(rec['filefolderpath'].Value):
+                    downloadfile.filefolderpath = self._transToAbsolutePath(pattern.sub(' ', row['filefolderpath'].Value)) if 'filefolderpath' in row and row['filefolderpath'].Value is not '' else None
                 downloadfile.totalsize = row['totalsize'].Value if 'totalsize' in row else None
-                downloadfile.createdate = self._timeHandler(row['creatdate'].Value) if 'totalsize' in row else None
-                downloadfile.donedate = self._timeHandler(row['donedate'].Value) if 'donedate' in row else None
+                if not IsDBNull(rec['creatdate'].Value):
+                    downloadfile.createdate = self._timeHandler(row['creatdate'].Value) if 'totalsize' in row else None
+                if not IsDBNull(rec['donedate'].Value):
+                    downloadfile.donedate = self._timeHandler(row['donedate'].Value) if 'donedate' in row else None
                 downloadfile.costtime = row['costtime'].Value if 'costtime' in row else None
                 downloadfile.source = node.AbsolutePath
                 downloadfile.deleted = 1
                 self.mb.db_insert_table_downloadfiles(downloadfile)
             self.mb.db_commit()
         except Exception as e:
+            traceback.print_exc()
             print(e)
 
     def analyze_fileinfo(self):
@@ -149,15 +163,18 @@ class QQBrowserParse(object):
                 for rec in self.db.ReadTableRecords(ts, self.extractDeleted, True):
                     canceller.ThrowIfCancellationRequested()
                     fileinfo.id = rec['FILE_ID'].Value if 'FILE_ID' in rec else None
-                    fileinfo.filepath = self._transToAbsolutePath(pattern.sub('', rec['FILE_PATH'].Value)) if 'FILE_PATH' in rec and rec['FILE_PATH'].Value is not '' else None
+                    if not IsDBNull(rec['FILE_PATH'].Value):
+                        fileinfo.filepath = self._transToAbsolutePath(pattern.sub('', rec['FILE_PATH'].Value)) if 'FILE_PATH' in rec and rec['FILE_PATH'].Value is not '' else None
                     fileinfo.filename = pattern.sub('', rec['FILE_NAME'].Value) if 'FILE_NAME' in rec and rec['FILE_NAME'].Value is not '' else None
                     fileinfo.size = rec['SIZE'].Value if 'SIZE' in rec else None
-                    fileinfo.modified = self._timeHandler(rec['MODIFIED_DATE'].Value) if 'MODIFIED_DATE' in rec else None
+                    if not IsDBNull(rec['MODIFIED_DATE'].Value):
+                        fileinfo.modified = self._timeHandler(rec['MODIFIED_DATE'].Value) if 'MODIFIED_DATE' in rec else None
                     fileinfo.title = pattern.sub('', rec['TITLE'].Value) if 'TITLE' in rec and rec['TITLE'].Value is not '' else None
                     fileinfo.source = node.AbsolutePath
                     self.mb.db_insert_table_fileinfos(fileinfo)
                 self.mb.db_commit()
         except Exception as e:
+            traceback.print_exc()
             print(e)
 
     def analyze_search_history(self):
@@ -179,7 +196,8 @@ class QQBrowserParse(object):
                     searchHistory.id = rec['ID'].Value if 'ID' in rec else None
                     searchHistory.name = rec['NAME'].Value if 'NAME' in rec and rec['NAME'].Value is not '' else None
                     searchHistory.url = rec['URL'].Value if 'URL' in rec and rec['URL'].Value is not '' else None
-                    searchHistory.datetime = self._timeHandler(rec['DATETIME'].Value) if 'DATETIME' in rec else None
+                    if not IsDBNull(rec['DATETIME'].Value):
+                        searchHistory.datetime = self._timeHandler(rec['DATETIME'].Value) if 'DATETIME' in rec else None
                     users = re.findall(r'^.*/(.+).db', node.AbsolutePath)
                     searchHistory.owneruser = users[0]
                     searchHistory.source = node.AbsolutePath
@@ -190,7 +208,8 @@ class QQBrowserParse(object):
                     searchHistory.id = row['ID'].Value if 'ID' in row else None
                     searchHistory.name = pattern.sub('', row['NAME'].Value) if 'NAME' in row and row['NAME'].Value is not '' else None
                     searchHistory.url = pattern.sub('', row['URL'].Value) if 'URL' in row and row['URL'].Value is not '' else None
-                    searchHistory.datetime = self._timeHandler(row['DATETIME'].Value) if 'DATETIME' in row else None
+                    if not IsDBNull(rec['DATETIME'].Value):
+                        searchHistory.datetime = self._timeHandler(row['DATETIME'].Value) if 'DATETIME' in row else None
                     users = re.findall(r'^.*/(.+).db', node.AbsolutePath)
                     searchHistory.owneruser = users[0]
                     searchHistory.source = node.AbsolutePath
@@ -198,6 +217,7 @@ class QQBrowserParse(object):
                     self.mb.db_insert_table_searchhistory(searchHistory)
                 self.mb.db_commit()
         except Exception as e:
+            traceback.print_exc()
             print(e)
 
     def analyze_accounts(self):
@@ -217,6 +237,7 @@ class QQBrowserParse(object):
                 self.mb.db_insert_table_accounts(account)
                 self.mb.db_commit()
         except Exception as e:
+            traceback.print_exc()
             print(e)
 
     def analyze_plugin(self):
@@ -239,6 +260,7 @@ class QQBrowserParse(object):
                 self.mb.db_insert_table_plugin(plugin)
             self.mb.db_commit()
         except Exception as e:
+            traceback.print_exc()
             print(e)
 
     def analyze_cookies(self):
@@ -254,14 +276,18 @@ class QQBrowserParse(object):
                 cookie.host_key = rec['host_key'].Value if 'host_key' in rec and rec['host_key'].Value is not '' else None
                 cookie.name  = rec['name'].Value if 'name' in rec and rec['name'].Value is not '' else None
                 cookie.value = rec['value'].Value if 'value' in rec and rec['value'].Value is not '' else None
-                cookie.createdate = self._timeHandler(rec['creation_utc'].Value) if 'creation_utc' in rec else None
-                cookie.expiredate = self._timeHandler(rec['expires_utc'].Value) if 'expires_utc' in rec else None
-                cookie.lastaccessdate = self._timeHandler(rec['last_access_utc'].Value) if 'last_access_utc' in rec else None
+                if not IsDBNull(rec['creation_utc'].Value):
+                    cookie.createdate = self._timeHandler(rec['creation_utc'].Value) if 'creation_utc' in rec else None
+                if not IsDBNull(rec['expires_utc'].Value):
+                    cookie.expiredate = self._timeHandler(rec['expires_utc'].Value) if 'expires_utc' in rec else None
+                if not IsDBNull(rec['last_access_utc'].Value):
+                    cookie.lastaccessdate = self._timeHandler(rec['last_access_utc'].Value) if 'last_access_utc' in rec else None
                 cookie.hasexipred = rec['has_expires'].Value if 'has_expires' in rec else None
                 cookie.source = node.AbsolutePath
                 self.mb.db_insert_table_cookies(cookie)
             self.mb.db_commit()
         except Exception as e:
+            traceback.print_exc()
             print(e)
 
     def _timeHandler(self, time):
@@ -306,7 +332,7 @@ class QQBrowserParse(object):
         self.analyze_browserecords()
         self.analyze_cookies()
         self.analyze_downloadfiles()
-        #self.analyze_fileinfo()
+        self.analyze_fileinfo()
         self.analyze_plugin()
         self.analyze_search_history()
         self.mb.db_close()
