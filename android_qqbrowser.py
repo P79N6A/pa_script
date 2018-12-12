407edd710d6eb08b07fbdc99ddb15bf74fbdfecd
yuge <xyg@pwnzen.com>
2018-12-12 15:18:52 +0800

    fix

--- android_qqbrowser.py
+++ android_qqbrowser.py
@@ -21,7 +21,7 @@ import re
 import traceback
 import hashlib
 
-VERSION_APP_VALUE = 1
+VERSION_APP_VALUE = 3
 
 class QQBrowserParse(object):
     def __init__(self, node, extractDeleted, extractSource):
@@ -30,6 +30,13 @@ class QQBrowserParse(object):
         self.extractSource = extractSource
         self.db = None
         self.mb = MB()
+        fs = self.node.FileSystem
+        nodes = list(fs.Search('/com.tencent.mtt/files$'))
+        self.downloadfilesnodes1 = nodes[0] if len(nodes) != 0 else None
+        nodes = list(fs.Search('/storage/emulated/0/QQBrowser$'))
+        self.downloadfilesnodes2 = nodes[0] if len(nodes) != 0 else None
+        nodes = list(fs.Search('/data/media/0/QQBrowser$'))
+        self.downloadfilesnodes3 = nodes[0] if len(nodes) !=0 else None
         self.cache_path = ds.OpenCachePath("QQBrowser")
         md5_db = hashlib.md5()
         db_name = 'QQBrowser'
@@ -61,19 +68,7 @@ class QQBrowserParse(object):
                     users = re.findall(r'^.*/(.+).db', node.AbsolutePath)
                     bookmark.owneruser = users[0]
                     bookmark.source = self.node.AbsolutePath
-                    self.mb.db_insert_table_bookmarks(bookmark)
-                self.mb.db_commit()
-                for row in self.db.ReadTableDeletedRecords(ts, False):
-                    canceller.ThrowIfCancellationRequested()
-                    bookmark.id = row['_id'].Value if '_id' in row else None
-                    if not IsDBNull(rec['created'].Value):
-                        bookmark.time = self._timeHandler(row['created'].Value) if 'created' in row else None
-                    bookmark.title = pattern.sub('', row['title'].Value) if 'title' in row and row['title'].Value is not '' else None
-                    bookmark.url = pattern.sub('', row['url'].Value) if 'url' in row and row['url'].Value is not '' else None
-                    users = re.findall(r'^.*/(.+).db', node.AbsolutePath)
-                    bookmark.owneruser = users[0]
-                    bookmark.source = self.node.AbsolutePath
-                    bookmark.deleted = 1
+                    bookmark.deleted = rec.IsDeleted
                     self.mb.db_insert_table_bookmarks(bookmark)
                 self.mb.db_commit()
         except Exception as e:
@@ -96,18 +91,8 @@ class QQBrowserParse(object):
                 record.url = rec['URL'].Value if 'URL' in rec and rec['URL'].Value is not '' else None
                 if not IsDBNull(rec['DATETIME'].Value):
                     record.datetime = self._timeHandler(rec['DATETIME'].Value) if 'DATETIME' in rec else None
-                self.source = node.AbsolutePath
-                self.mb.db_insert_table_browserecords(record)
-            self.mb.db_commit()
-            for row in self.db.ReadTableDeletedRecords(ts, False):
-                canceller.ThrowIfCancellationRequested()
-                record.id = row['ID'].Value if 'ID' in row else None
-                record.name = row['NAME'].Value if 'NAME' in row and row['NAME'].Value is not '' else None
-                record.url = row['URL'].Value if 'URL' in row and row['URL'].Value is not '' else None
-                if not IsDBNull(rec['DATETIME'].Value):
-                    record.datetime = self._timeHandler(row['DATETIME'].Value) if 'DATETIME' in row else None
+                record.deleted = rec.IsDeleted
                 record.source = node.AbsolutePath
-                record.deleted = 1
                 self.mb.db_insert_table_browserecords(record)
             self.mb.db_commit()
         except Exception as e:
@@ -128,32 +113,16 @@ class QQBrowserParse(object):
                 downloadfile.id = rec['id'].Value if 'id' in rec else None
                 downloadfile.url = rec['url'].Value if 'url' in rec and rec['url'].Value is not '' else None
                 downloadfile.filename = rec['filename'].Value if 'filename' in rec and rec['filename'].Value is not '' else None
-                if not IsDBNull(rec['filefolderpath'].Value):
-                    downloadfile.filefolderpath = self._transToAbsolutePath(rec['filefolderpath'].Value) if 'filefolderpath' in rec and rec['filefolderpath'].Value is not '' else None
+                if not IsDBNull(rec['filename'].Value):
+                    downloadfile.filefolderpath = self._transToAbsolutePath(rec['filename'].Value) if 'filename' in rec and rec['filename'].Value is not '' else None
                 downloadfile.totalsize = rec['totalsize'].Value if 'totalsize' in rec else None
                 if not IsDBNull(rec['createdate'].Value):
                     downloadfile.createdate = self._timeHandler(rec['createdate'].Value) if 'createdate' in rec else None
                 if not IsDBNull(rec['donedate'].Value):
                     downloadfile.donedate = self._timeHandler(rec['donedate'].Value) if 'donedate' in rec else None
                 downloadfile.costtime = rec['costtime'].Value if 'costtime' in rec else None
+                downloadfile.deleted = rec.IsDeleted
                 downloadfile.source = node.AbsolutePath
-                self.mb.db_insert_table_downloadfiles(downloadfile)
-            self.mb.db_commit()
-            for row in self.db.ReadTableDeletedRecords(ts, False):
-                canceller.ThrowIfCancellationRequested()
-                downloadfile.id = row['id'].Value if 'id' in row else None
-                downloadfile.url = row['url'].Value if 'url' in row and row['url'].Value is not '' else None
-                downloadfile.filename = row['filename'].Value if 'filename' in row and row['filename'].Value is not '' else None
-                if not IsDBNull(rec['filefolderpath'].Value):
-                    downloadfile.filefolderpath = self._transToAbsolutePath(pattern.sub(' ', row['filefolderpath'].Value)) if 'filefolderpath' in row and row['filefolderpath'].Value is not '' else None
-                downloadfile.totalsize = row['totalsize'].Value if 'totalsize' in row else None
-                if not IsDBNull(rec['creatdate'].Value):
-                    downloadfile.createdate = self._timeHandler(row['creatdate'].Value) if 'totalsize' in row else None
-                if not IsDBNull(rec['donedate'].Value):
-                    downloadfile.donedate = self._timeHandler(row['donedate'].Value) if 'donedate' in row else None
-                downloadfile.costtime = row['costtime'].Value if 'costtime' in row else None
-                downloadfile.source = node.AbsolutePath
-                downloadfile.deleted = 1
                 self.mb.db_insert_table_downloadfiles(downloadfile)
             self.mb.db_commit()
         except Exception as e:
@@ -185,6 +154,7 @@ class QQBrowserParse(object):
                     if not IsDBNull(rec['TITLE'].Value):
                         fileinfo.title = pattern.sub('', rec['TITLE'].Value) if 'TITLE' in rec and rec['TITLE'].Value is not '' else None
                     fileinfo.source = node.AbsolutePath
+                    fileinfo.deleted = rec.IsDeleted
                     self.mb.db_insert_table_fileinfos(fileinfo)
                 self.mb.db_commit()
         except Exception as e:
@@ -216,19 +186,7 @@ class QQBrowserParse(object):
                     users = re.findall(r'^.*/(.+).db', node.AbsolutePath)
                     searchHistory.owneruser = users[0]
                     searchHistory.source = node.AbsolutePath
-                    self.mb.db_insert_table_searchhistory(searchHistory)
-                self.mb.db_commit()
-                for row in self.db.ReadTableDeletedRecords(ts, False):
-                    canceller.ThrowIfCancellationRequested()
-                    searchHistory.id = row['ID'].Value if 'ID' in row else None
-                    searchHistory.name = pattern.sub('', row['NAME'].Value) if 'NAME' in row and row['NAME'].Value is not '' else None
-                    searchHistory.url = pattern.sub('', row['URL'].Value) if 'URL' in row and row['URL'].Value is not '' else None
-                    if not IsDBNull(rec['DATETIME'].Value):
-                        searchHistory.datetime = self._timeHandler(row['DATETIME'].Value) if 'DATETIME' in row else None
-                    users = re.findall(r'^.*/(.+).db', node.AbsolutePath)
-                    searchHistory.owneruser = users[0]
-                    searchHistory.source = node.AbsolutePath
-                    searchHistory.deleted = 1
+                    searchHistory.deleted = rec.IsDeleted
                     self.mb.db_insert_table_searchhistory(searchHistory)
                 self.mb.db_commit()
         except Exception as e:
@@ -275,6 +233,7 @@ class QQBrowserParse(object):
                 plugin.packagesize = rec['packageSize'].Value if 'packageSize' in rec and rec['packageSize'].Value is not '' else None
                 plugin.isinstall = rec['isInstall'].Value if 'isInstall' in rec else None
                 plugin.source = node.AbsolutePath
+                plugin.deleted = rec.IsDeleted
                 self.mb.db_insert_table_plugin(plugin)
             self.mb.db_commit()
         except Exception as e:
@@ -302,6 +261,7 @@ class QQBrowserParse(object):
                     cookie.lastaccessdate = rec['last_access_utc'].Value if 'last_access_utc' in rec else None
                 cookie.hasexipred = rec['has_expires'].Value if 'has_expires' in rec else None
                 cookie.source = node.AbsolutePath
+                cookie.deleted = rec.IsDeleted
                 self.mb.db_insert_table_cookies(cookie)
             self.mb.db_commit()
         except Exception as e:
@@ -316,33 +276,27 @@ class QQBrowserParse(object):
         else:
             return 0
         
-    def _transToAbsolutePath(self, dir):
+    def _transToAbsolutePath(self, filename):
         fs = self.node.FileSystem
         try:
-            if re.match(r'^/storage/emulated/0', dir) is not None:
-                subdir = dir.replace('/storage/emulated/0', '')
-                fileNode = fs.Search(subdir + '$')
-                for node in fileNode:
-                    return node.AbsolutePath
-            elif  re.match(r'^/data/user/0', dir) is not None:
-                subdir = dir.replace('/data/user/0', '')
-                fileNode = fs.Search(subdir + '$')
-                for node in fileNode:
-                    return node.AbsolutePath
-            elif re.match(r'^/storage/0000-0000', dir) is not None:
-                fileNode = fs.Search(subdir + '$')
-                for node in fileNode:
-                    return node.AbsolutePath
-            elif re.match(r'^/$', dir) is not None:
-                return fs.AbsolutePath
-            elif '*' not in dir:
-                fileNode = fs.Search(dir+ '$')
-                for node in fileNode:
-                    return node.AbsolutePath
+            nodes1 = list(self.downloadfilesnodes1.Search(filename + '$')) if self.downloadfilesnodes1 is not None else []
+            nodes2 = list(self.downloadfilesnodes2.Search(filename + '$')) if self.downloadfilesnodes2 is not None else []
+            nodes3 = list(self.downloadfilesnodes3.Search(filename + '$')) if self.downloadfilesnodes3 is not None else []
+            if len(nodes1) != 0:
+                return nodes1[0].AbsolutePath
+            elif len(nodes2) != 0:
+                return nodes2[0].AbsolutePath
+            elif len(nodes3) != 0:
+                return nodes3[0].AbsolutePath
             else:
                 return None
+                #nodes = list(fs.Search(filename + '$'))
+                #if len(nodes) == 0:
+                #    return None
+                #else:
+                #    return nodes[0].AbsolutePath
         except:
-            pass
+            traceback.print_exc()
 
     def parse(self):
         if self.mb.need_parse(self.db_cache, VERSION_APP_VALUE):
@@ -352,7 +306,7 @@ class QQBrowserParse(object):
             self.analyze_browserecords()
             self.analyze_cookies()
             self.analyze_downloadfiles()
-            self.analyze_fileinfo()
+            #self.analyze_fileinfo()
             self.analyze_plugin()
             self.analyze_search_history()
             self.mb.db_insert_table_version(model_browser.VERSION_KEY_DB, model_browser.VERSION_VALUE_DB)
