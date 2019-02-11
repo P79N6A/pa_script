26e65068415f097c4a753e9da7c2eebf1fab1bed
yuge <xyg@pwnzen.com>
2019-02-11 15:44:28 +0800

    增加对附件解析

--- apple_notes.py
+++ apple_notes.py
@@ -2,6 +2,7 @@
 import os
 import PA_runtime
 from PA_runtime import *
+from PA.InfraLib.Utils import ConvertHelper
 
 def analyze_notes(node, extractDeleted, extractSource):
     pr = ParserResults()
@@ -29,6 +30,21 @@ def analyze_notes(node, extractDeleted, extractSource):
             SQLiteParser.Tools.ReadColumnToField(record, "ZCONTENT", res.Body, extractSource)
             pr.Models.Add(res)
 
+    attach_dic = {}
+    ts = SQLiteParser.TableSignature('ZNOTEATTACHMENT')
+    for rec in db.ReadTableRecords(ts, extractDeleted, True):
+        try:
+            note_id = rec['ZNOTE'].Value if 'ZNOTE' in rec and not IsDBNull(rec['ZNOTE'].Value) else 0
+            attach_id = rec['Z_PK'].Value if 'Z_PK' in rec and not IsDBNull(rec['Z_PK'].Value) else 0
+            if note_id == 0 or attach_id == 0:
+                continue
+            if note_id not in attach_dic.keys():
+                attach_dic[note_id] = [attach_id,]
+            else:
+                attach_dic[note_id].append(attach_id)
+        except:
+            pass
+
     ts = SQLiteParser.TableSignature('ZNOTE')
     if extractDeleted:
         ts['ZTITLE'] = TextNotNull
@@ -36,6 +52,7 @@ def analyze_notes(node, extractDeleted, extractSource):
         ts['ZMODIFICATIONDATE'] = ts['ZCREATIONDATE'] = SQLiteParser.Signatures.NumericSet(4, 7)
         ts['Z_OPT'] = ts['ZCONTAINSCJK'] = ts['ZEXTERNALFLAGS'] = ts['ZDELETEDFLAG'] = SQLiteParser.Signatures.NumericSet(1)
 
+    attach_node = node.Parent.GetByPath('/attachments')
     
     for record in db.ReadTableRecords(ts, extractDeleted, True):
         if not record['ZBODY'].Value in body_dic:
@@ -71,9 +88,29 @@ def analyze_notes(node, extractDeleted, extractSource):
                     res.Modification.Source = MemoryRange(record['ZMODIFICATIONDATE'].Source)
             except:
                 pass
+        if not IsDBNull(record['Z_PK'].Value):
+            try:
+                note_id = record['Z_PK'].Value
+                attach_ids = attach_dic[note_id] if note_id in attach_dic else []
+                for attach_id in attach_ids:
+                    dir_node = attach_node.Search('.*' + str(attach_id) + '$')
+                    if len(list(dir_node)) != 0:
+                        attach = Attachment()
+                        file_node = list(dir_node)[0].Search('.*\..*')
+                        if len(list(file_node)) != 0:
+                            attach.Uri.Value = get_uri(list(file_node)[0].AbsolutePath)
+                            res.Attachments.Add(attach)
+            except:
+                pass
         pr.Models.Add(res)
     pr.Build('备忘录')
     return pr
+
+def get_uri(path):
+        if path.startswith('http') or len(path) == 0:
+            return ConvertHelper.ToUri(path)
+        else:
+            return ConvertHelper.ToUri(path)
 
 def analyze_old_notes(node, extractDeleted, extractSource):
     db = SQLiteParser.Database.FromNode(node)
