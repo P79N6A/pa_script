b3d36bf4ef53cd614993830a264dc6d2dd204c49
yuge <xyg@pwnzen.com>
2019-03-19 15:41:06 +0800

    fix

--- apple_notes.py
+++ apple_notes.py
@@ -4,6 +4,7 @@ import PA_runtime
 from PA_runtime import *
 from PA.InfraLib.Utils import ConvertHelper
 import re
+import zlib
 
 def analyze_notes(node, extractDeleted, extractSource):
     pr = ParserResults()
@@ -185,6 +186,15 @@ def analyze_old_notes(node, extractDeleted, extractSource):
     return pr
 
 def analyze_notestore(node, extractDeleted):
+    '''
+    ZICNOTEDATA记录备忘录的正文内容与备忘id，正文内容以十六进制保存为zip
+    ZICCLOUDSYNCINGOBJECT记录备忘录的标题、附件、图片、文件夹等数据
+    在ZICCLOUDSYNCINGOBJECT表中：
+        ZACCOUNTTYPE字段不为空的数据为备忘录的用户数据
+        ZFOLDERTYPE字段不为空的数据为文件夹数据
+        ZSTATEMODIFICATIONDATE字段不为空的数据为设备迁移状态的数据（未解析）
+        ZATTACHMENT字段不为空的数据为备忘录的附件数据
+    '''
     try:
         notestore_node = node.FileSystem.Search('/group.com.apple.notes/NoteStore.sqlite$')
         if len(list(notestore_node)) == 0:
@@ -194,34 +204,187 @@ def analyze_notestore(node, extractDeleted):
         db = SQLiteParser.Database.FromNode(notestore_node[0])
         if db is None:
             return
-        ts = SQLiteParser.TableSignature('ZICCLOUDSYNCINGOBJECT')
-        ress = []
+        ts = SQLiteParser.TableSignature('ZICNOTEDATA')
+        #建立pk与内容的映射关系
+        pk2data = {}
+        #建立id与内容的映射关系
+        id2data = {}
+        #建立pk与id的映射关系
+        pk2id = {}
+        #建立id与pk的映射关系
+        id2pk = {}
         for record in db.ReadTableRecords(ts, extractDeleted, True):
             try:
-                res = Note()
-                res.Deleted = record.Deleted
-                if not IsDBNull(record['ZNESTEDTITLEFORSORTING'].Value):
-                    res.Title.Value = record['ZNESTEDTITLEFORSORTING'].Value
-                else:
-                    res.Title.Value = ''
-                if not IsDBNull(record['ZTITLE1'].Value):
-                    res.Body.Value = record['ZTITLE1'].Value
-                else:
-                    res.Body.Value = ''
-                if not IsDBNull(record['ZCREATIONDATE1'].Value):
-                    try:
-                        res.Creation.Value = TimeStamp(epoch.AddSeconds(record['ZCREATIONDATE1'].Value), True)
-                    except:
-                        pass
-                if not IsDBNull(record['ZMODIFICATIONDATE1'].Value):
-                    try:
-                        res.Modification.Value = TimeStamp(epoch.AddSeconds(record['ZMODIFICATIONDATE1'].Value), True)
-                    except:
-                        pass
-                if not res.Body.Value == '':
-                    ress.append(res)
+                pk = _db_record_get_int_value(record, 'Z_PK')
+                if pk == 0:
+                    continue
+                note_id = _db_record_get_int_value(record, 'ZNOTE')
+                data = record['ZDATA'].Value if not IsDBNull(record['ZDATA'].Value) else []
+                data = parse_gzip(data)
+                pk2data[pk] = data
+                id2data[note_id] = data
+                pk2id[pk] = note_id
+                id2pk[note_id] = pk
             except:
                 pass
-        return ress
+        ts = SQLiteParser.TableSignature('ZICCLOUDSYNCINGOBJECT')
+        #建立note_id与备忘录信息的对应关系{noteid:{folder:folderid, create_date:data, modify_date:data, title:data}, ...}
+        id2info = {}
+        #获取附件id与附件信息的对应关系{attachid:data, ...}
+        attachid2attachinfo = {}
+        #获取附件id与noteid的对应关系{noteid:[attachid1, attachid2, ...], ...}
+        noteid2attachid = {}
+        #建立note与folder之间的对应{folderid:foldername, ...}
+        folder_info = {}
+        for record in db.ReadTableRecords(ts, extractDeleted, True):
+            try:
+                if not IsDBNull(record['ZNOTEDATA'].Value):
+                    note_data = {}
+                    note_id = _db_record_get_int_value(record, 'ZNOTEDATA')
+                    folder_id = _db_record_get_int_value(record, 'ZFOLDER')
+                    create_date = _db_record_get_int_value(record, 'ZCREATIONDATE1')
+                    modify_date = _db_record_get_int_value(record, 'ZMODIFICATIONDATE1')
+                    title = _db_record_get_string_value(record, 'ZTITLE1')
+                    subject = _db_record_get_string_value(record, 'ZSNIPPET')
+                    note_data['folder'] = folder_id
+                    note_data['create_date'] = format_mac_timestamp(create_date)
+                    note_data['modify_date'] = format_mac_timestamp(modify_date)
+                    note_data['title'] = title
+                    note_data['subject'] = subject
+                    note_data['deleted'] = record.IsDeleted
+                    if note_id in id2info:
+                        id2info[note_id] = dict(id2info[note_id], **note_data)
+                    else:
+                        id2info[note_id] = note_data
+                elif not IsDBNull(record['ZATTACHMENT'].Value):
+                    attachment_id = _db_record_get_int_value(record, 'ZATTACHMENT')
+                    attachment_name = _db_record_get_string_value(record, 'ZIDENTIFIER')
+                    attachment_nodes = notestore_node[0].Parent.Search('/'+ attachment_name + '\..*$')
+                    if len(list(attachment_nodes)) != 0 and attachment_id not in attachid2attachinfo:
+                        attachid2attachinfo[attachment_id] = [attachment_nodes[0].AbsolutePath, attachment_nodes[0], attachment_name]
+                elif not IsDBNull(record['ZFOLDERTYPE'].Value):
+                    folder_pk = _db_record_get_int_value(record, 'Z_PK')
+                    folder_name = _db_record_get_string_value(record, 'ZNESTEDTITLEFORSORTING')
+                    folder_info[folder_pk] = folder_name
+                elif not IsDBNull(record['ZNOTE'].Value):
+                    attach_id = _db_record_get_int_value(record, 'Z_PK')
+                    note_id = _db_record_get_int_value(record, 'ZNOTE')
+                    if note_id in noteid2attachid:
+                        noteid2attachid[note_id].append(attach_id)
+                    else:
+                        noteid2attachid[note_id] = [attach_id,]
+            except Exception as e:
+                print(e)
+        models = []
+        for key, value in id2info.items():
+            note_id = pk2id[key]
+            res = Note()
+            res.Title.Value = value['title'] if 'title' in value else None
+            res.Summary.Value = value['subject'] if 'subject' in value else None
+            res.Body.Value = pk2data[key]
+            res.Creation.Value = _get_timestamp(value['create_date'])
+            res.Modification.Value = _get_timestamp(value['modify_date'])
+            res.Folder.Value = folder_info[value['folder']]
+            res.SourceFile.Value = notestore_node[0].AbsolutePath.replace('/', '\\')
+            res.Deleted = _convert_deleted_status(value['deleted'])
+            try:
+                if note_id in noteid2attachid:
+                    for attach_id in noteid2attachid[note_id]:
+                        if attach_id in attachid2attachinfo:
+                            attachment = Attachment()
+                            attachment.Uri.Value = ConvertHelper.ToUri(attachid2attachinfo[attach_id][0])
+                            attach_node = attachid2attachinfo[attach_id][1]
+                            attach_name = attachid2attachinfo[attach_id][2]
+                            attachment.Filename.Value = attach_name
+                            src = '<img src="' + attach_node.PathWithMountPoint + '" width="80%"  height="80%" />'
+                            res.Attachments.Add(attachment)
+                            res.Body.Value += src
+            except Exception as e:
+                pass
+            models.append(res)
+        return models
     except Exception as e:
         print(e)
+
+def parse_gzip(data):
+    try:
+        if not data:
+            return None
+        decompressed_data=zlib.decompress(data, 16+zlib.MAX_WBITS)
+        start = decompressed_data.find('\x08\x00\x10\x00\x1a')+5
+        end = decompressed_data.find('\x1a\x10\x0a\x04\x08')+1
+        content = decompressed_data[start: end]
+        start = content.find('\x12')+2
+        content = content[start: end].replace('\xef\xbf\xbc', '')
+        content = content.decode("utf-8", "ignore")
+        return re.sub('[\\x00-\\x08\\x0b-\\x0c\\x0e-\\x1f]', '', content)
+    except:
+        return None
+
+def _db_record_get_value(record, column, default_value=None):
+    if not record[column].IsDBNull:
+        return record[column].Value
+    return default_value
+
+def _db_record_get_string_value(record, column, default_value=''):
+    if not record[column].IsDBNull:
+        try:
+            value = str(record[column].Value)
+            #if record.Deleted != DeletedState.Intact:
+            #    value = filter(lambda x: x in string.printable, value)
+            return value
+        except Exception as e:
+            return default_value
+    return default_value
+
+def _db_record_get_int_value(record, column, default_value=0):
+    if not record[column].IsDBNull:
+        try:
+            return int(record[column].Value)
+        except Exception as e:
+            return default_value
+    return default_value
+
+def _db_record_get_blob_value(record, column, default_value=None):
+    if not record[column].IsDBNull:
+        try:
+            value = record[column].Value
+            return bytearray(value)
+        except Exception as e:
+            return default_value
+    return default_value
+
+def format_mac_timestamp(mac_time, v = 10):
+    """
+    from mac-timestamp generate unix time stamp
+    """
+    try:
+        date = 0
+        date_2 = mac_time
+        if mac_time < 1000000000:
+            date = mac_time + 978307200
+        else:
+            date = mac_time
+            date_2 = date_2 - 978278400 - 8 * 3600
+        s_ret = date if v > 5 else date_2
+        return int(s_ret)
+    except:
+        return None
+
+def _convert_deleted_status(deleted):
+    if deleted is None:
+        return DeletedState.Unknown
+    else:
+        return DeletedState.Intact if deleted == 0 else DeletedState.Deleted
+
+def _get_timestamp(timestamp):
+    try:
+        if isinstance(timestamp, (long, float, str, Int64)) and len(str(timestamp)) > 10:
+            timestamp = int(str(timestamp)[:10])
+        if isinstance(timestamp, int) and len(str(timestamp)) == 10:
+            ts = TimeStamp.FromUnixTime(timestamp, False)
+            if not ts.IsValidForSmartphone():
+                ts = None
+            return ts
+    except:
+        return None
